f871db792c5e70e15d7fe568c03d071aa2880a59
zxyzds <zxy_zds@mail.ustc.edu.cn>
2026-01-20 04:00:18 +0800

    Update README and include spard code

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # SPARD
+SPARD: Self-Paced Curriculum for RL Alignment via Integrating
+Reward Dynamics and Data Utility
+## Method
+[![method](figures/method.png)](figures/method.png)
+The framework of SPARD consists of two main synergistic mechanisms: (1) Progress-Aware
+Weight Adaptation dynamically adjusts reward weights (wr ) based on the reliability of performance gains, and
+(2) Reward-Attributed Data Rebalancing computes data weights (wd) by aggregating reward importance via a
+reward-attribute matrix derived from score dispersion. These components jointly guide the optimization to prioritize
+current learning objectives and leverage the most efficient data.
+
+## Execution Instructions
+For the training process, we used verl for its high efficiency and scalability. We use DeepSeek-R1 as the judge (scoring) model for evaluation. To run evaluation, you need a model service endpoint, either a hosted API (e.g., OpenAI or DeepSeek official API), or a locally deployed service via LMDeploy, vLLM, or SGLang. Then set the environment variables below and run the evaluation scripts:
+
+```bash
+export JUDGE_MODEL=<MODEL_NAME>
+export JUDGE_API_KEY=<YOUR_API_KEY>
+export JUDGE_API_BASE=<API_BASE_URL>
+```
+
+Command to execute:
+```bash
+bash scirpts/spard.sh
+```
---
+++ scirpts/spard.sh
@@ -0,0 +1,51 @@
+export WANDB_API_KEY=<Your WANDB_API_KEY>
+
+# 多机训练设置
+export NPROC_PER_NODE=8
+export NNODES=$WORLD_SIZE
+export NODE_RANK=$RANK
+
+swift rlhf \
+    --rlhf_type grpo \
+    --model '<MODEL_NAME>' \
+    --external_plugins   \
+    --reward_funcs  '<reward funcs>' \
+    --reward_weights '<a list for reward funcs weights>' \
+    --dynaopt_enabled true \
+    --dynaopt_weight_enabled true \
+    --dynaopt_data_enabled true \
+    --dynaopt_every_steps 4 \
+    --reward_thread_num 4 \
+    --train_type full \
+    --use_vllm true \
+    --vllm_mode colocate \
+    --vllm_gpu_memory_utilization 0.4  \
+    --vllm_max_model_len  12000 \
+    --vllm_tensor_parallel_size 4  \
+    --torch_dtype bfloat16 \
+    --dataset datasets/grpo_data.jsonl \
+    --max_completion_length 8192 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 1 \
+    --learning_rate 1e-6 \
+    --gradient_accumulation_steps 1 \
+    --eval_steps 200 \
+    --save_steps 30 \
+    --save_total_limit 20 \
+    --logging_steps 1 \
+    --max_length 12000 \
+    --output_dir '<output_path>' \
+    --warmup_ratio 0.05 \
+    --dataloader_num_workers 4 \
+    --dataset_num_proc 4 \
+    --num_generations 8 \
+    --temperature 0.7 \
+    --deepspeed zero3 \
+    --sequence_parallel_size 4 \
+    --overlong_filter true \
+    --report_to tensorboard wandb \
+    --log_completions true \
+    --dynamic_sample true \
+    --log_entropy true \
+    --dataset_shuffle false
